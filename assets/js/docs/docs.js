c7a58f93db44ffee881c55807567784e3c0dbcc9
Andy Fleming <andy@andyfleming.com>
2015-12-17 20:09:11 -0800

    Refactoring to minimize number of requests

--- assets/js/docs/docs.js
+++ assets/js/docs/docs.js
@@ -14,42 +14,47 @@ $(function() {
         // https://api.github.com/repos/space-race/mc-core/milestones
         // https://api.github.com/repos/space-race/mc-core/issues?state=open&milestone=1
 
-        $.getJSON('https://api.github.com/repos/' + repo + '/milestones', function(milestones) {
-            milestones.forEach(function(ms) {
-                var percentageComplete = Math.round(ms.closed_issues / (ms.closed_issues + ms.open_issues)*100);
-                var percentageToDisplay = (percentageComplete >= 8) ? percentageComplete : 8;
-                var $milestoneDiv = $('<div class="milestone"></div>');
-                var $milestoneProgressBar = $(
-                  '<div class="progress">' +
-                  '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="'+ percentageComplete +'" aria-valuemin="0" aria-valuemax="100" style="width: ' + percentageToDisplay + '%;">' +
-                  ms.closed_issues + ' / ' + (ms.closed_issues + ms.open_issues) +
-                  '</div>' +
-                  '</div>');
-                var $issueList = $('<ul></ul>');
-                $milestoneDiv.append(
-                  $('<h3></h3>')
-                    .append(
-                      $('<a></a>')
-                        .text(ms.title)
-                        .attr('href', ms.html_url)
-                        .attr('target', '_blank')
-                    )
-                );
-                $milestoneDiv.append($milestoneProgressBar);
-                $milestoneDiv.append($('<p></p>').text(ms.description));
-                $milestoneDiv.append($issueList);
-                $.getJSON('https://api.github.com/repos/' + repo + '/issues?state=open&milestone=' + ms.number, function(issues) {
+        $.getJSON('https://api.github.com/repos/' + repo + '/issues?state=open', function(issues) {
+
+            $.getJSON('https://api.github.com/repos/' + repo + '/milestones', function(milestones) {
+                milestones.forEach(function(ms) {
+                    var percentageComplete = Math.round(ms.closed_issues / (ms.closed_issues + ms.open_issues)*100);
+                    var percentageToDisplay = (percentageComplete >= 8) ? percentageComplete : 8;
+                    var $milestoneDiv = $('<div class="milestone"></div>');
+                    var $milestoneProgressBar = $(
+                      '<div class="progress">' +
+                      '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="'+ percentageComplete +'" aria-valuemin="0" aria-valuemax="100" style="width: ' + percentageToDisplay + '%;">' +
+                      ms.closed_issues + ' / ' + (ms.closed_issues + ms.open_issues) +
+                      '</div>' +
+                      '</div>');
+                    var $issueList = $('<ul></ul>');
+                    $milestoneDiv.append(
+                      $('<h3></h3>')
+                        .append(
+                          $('<a></a>')
+                            .text(ms.title)
+                            .attr('href', ms.html_url)
+                            .attr('target', '_blank')
+                        )
+                    );
+                    $milestoneDiv.append($milestoneProgressBar);
+                    $milestoneDiv.append($('<p></p>').text(ms.description));
+                    $milestoneDiv.append($issueList);
                     issues.forEach(function(issue) {
-                        $issueList.append(
-                          $('<li></li>').append(
-                            $('<a></a>').text(issue.title).attr('href', issue.html_url).attr('target','_blank')
-                          )
-                        );
+                        if (issue.milestone !== null && issue.milestone.number === ms.number) {
+                            $issueList.append(
+                              $('<li></li>').append(
+                                $('<a></a>').text(issue.title).attr('href', issue.html_url).attr('target','_blank')
+                              )
+                            );
+                        }
                     });
+                    $container.append($milestoneDiv);
                 });
-                $container.append($milestoneDiv);
             });
+
         });
+
 
     });
 
